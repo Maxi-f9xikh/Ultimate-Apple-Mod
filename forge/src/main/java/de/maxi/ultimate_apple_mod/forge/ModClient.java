2f0cb40e00baa57b9f483696221122b77f01c5eb
Maxi <red7creeper@gmail.com>
2026-04-23 03:16:15 +0200

    feat: add item colors, baby-zombie events, witch particles, and model JSONs

--- forge/src/main/java/de/maxi/ultimate_apple_mod/forge/ModClient.java
+++ forge/src/main/java/de/maxi/ultimate_apple_mod/forge/ModClient.java
@@ -1,6 +1,11 @@
 package de.maxi.ultimate_apple_mod.forge;
 
+import de.maxi.ultimate_apple_mod.item.AppleBombEntity;
+import net.minecraft.client.renderer.entity.ThrownItemRenderer;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.EntityRenderersEvent;
+import net.minecraftforge.client.event.RegisterColorHandlersEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 import static de.maxi.ultimate_apple_mod.ultimate_apple_mod.MOD_ID;
@@ -8,4 +13,19 @@ import static de.maxi.ultimate_apple_mod.ultimate_apple_mod.MOD_ID;
 @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class ModClient {
 
+    @SubscribeEvent
+    public static void onRegisterItemColors(RegisterColorHandlersEvent.Item event) {
+        event.register((stack, tintIndex) -> 0x9B00FF, ultimate_apple_modForge.GRAVITY_APPLE.get());
+        event.register((stack, tintIndex) -> 0x44CC44, ultimate_apple_modForge.ORCHARD_CALLER.get());
+        event.register((stack, tintIndex) -> 0x00CCDD, ultimate_apple_modForge.ECHO_APPLE.get());
+        event.register((stack, tintIndex) -> 0x00FF41, ultimate_apple_modForge.GLITCH_APPLE.get());
+        event.register((stack, tintIndex) -> 0xFF6600, ultimate_apple_modForge.APPLE_BOMB.get());
+        event.register((stack, tintIndex) -> 0xB87333, ultimate_apple_modForge.COPPER_APPLE.get());
+    }
+
+    @SubscribeEvent
+    public static void onRegisterEntityRenderers(EntityRenderersEvent.RegisterRenderers event) {
+        event.registerEntityRenderer(ultimate_apple_modForge.APPLE_BOMB_ENTITY.get(),
+            ThrownItemRenderer::new);
+    }
 }
