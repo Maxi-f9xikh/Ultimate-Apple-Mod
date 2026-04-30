eb08d7e809168e41e79ce9b58dfdf7ef458db392
Maxi <red7creeper@gmail.com>
2026-04-30 21:06:12 +0200

    Apple Color fix
    removed the colors from all the apples so the textures are fixed

--- forge/src/main/java/de/maxi/ultimate_apple_mod/forge/ModClient.java
+++ forge/src/main/java/de/maxi/ultimate_apple_mod/forge/ModClient.java
@@ -43,17 +43,6 @@ public class ModClient {
     }
 
     @SubscribeEvent
-    public static void onRegisterItemColors(RegisterColorHandlersEvent.Item event) {
-        event.register((stack, tintIndex) -> 0x00BBFF, ultimate_apple_modForge.REWIND_APPLE.get());
-        event.register((stack, tintIndex) -> 0xFF6600, ultimate_apple_modForge.APPLE_BOMB.get());
-        event.register((stack, tintIndex) -> 0xB87333, ultimate_apple_modForge.COPPER_APPLE.get());
-        event.register((stack, tintIndex) -> 0x3E1F5C, ultimate_apple_modForge.WITHER_APPLE.get());
-        event.register((stack, tintIndex) -> 0xFFAA22, ultimate_apple_modForge.HONEY_APPLE.get());
-        event.register((stack, tintIndex) -> 0xCC0000, ultimate_apple_modForge.DRAGON_APPLE.get());
-        event.register((stack, tintIndex) -> 0xFFFFFF, ultimate_apple_modForge.NETHER_STAR_APPLE.get());
-    }
-
-    @SubscribeEvent
     public static void onRegisterEntityRenderers(EntityRenderersEvent.RegisterRenderers event) {
         event.registerEntityRenderer(ultimate_apple_modForge.APPLE_BOMB_ENTITY.get(),
             ThrownItemRenderer::new);
